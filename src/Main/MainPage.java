5ca29200959c51073556c2a4e8f79e0751272ee2
rafliyulistiawan <rafliyulistiawan@gmail>
2024-06-15 01:26:47 +0700

    add color

--- src/Main/MainPage.java
+++ src/Main/MainPage.java
@@ -9,8 +9,11 @@ import javax.swing.*;
 public class MainPage extends JFrame {
 
   Graphics g;
-  String State = "";
+  String State = null;
   int lx, ly;
+  int scaleX, scaleY;
+  Color currentFillColor = Color.white;
+
   private int rotationAngle = 0;
 
   MainPage() {
@@ -37,22 +40,10 @@ public class MainPage extends JFrame {
         btnClearHandler(e);
       }
     });
-    btnPentagon.addActionListener(new ActionListener() {
-      @Override
-      public void actionPerformed(ActionEvent e) {
-        btnPentagonHandler(e);
-      }
-    });
     btnRectangle.addActionListener(new ActionListener() {
       @Override
       public void actionPerformed(ActionEvent e) {
         btnRectangleHandler(e);
-      }
-    });
-    btnHexagon.addActionListener(new ActionListener() {
-      @Override
-      public void actionPerformed(ActionEvent e) {
-        btnHexagonHandler(e);
       }
     });
     btnOval.addActionListener(new ActionListener() {
@@ -67,6 +58,36 @@ public class MainPage extends JFrame {
         btnTriangleHandler(e);
       }
     });
+    btnPentagon.addActionListener(new ActionListener() {
+      @Override
+      public void actionPerformed(ActionEvent e) {
+        btnPentagonHandler(e);
+      }
+    });
+    btnHexagon.addActionListener(new ActionListener() {
+      @Override
+      public void actionPerformed(ActionEvent e) {
+        btnHexagonHandler(e);
+      }
+    });
+    btnRed.addActionListener(new ActionListener() {
+      @Override
+      public void actionPerformed(ActionEvent e) {
+        btnColorHandler(e, Color.red);
+      }
+    });
+    btnGreen.addActionListener(new ActionListener() {
+      @Override
+      public void actionPerformed(ActionEvent e) {
+        btnColorHandler(e, Color.green);
+      }
+    });
+    btnBlue.addActionListener(new ActionListener() {
+      @Override
+      public void actionPerformed(ActionEvent e) {
+        btnColorHandler(e, Color.blue);
+      }
+    });
   }
 
   private void addComponents() {
@@ -76,12 +97,12 @@ public class MainPage extends JFrame {
     add(btnTranslate);
     add(btnRotate);
     add(btnScale);
-    
+
     add(btnRectangle);
     add(btnOval);
     add(btnTriangle);
-    add(btnPentagon);
-    add(btnHexagon);
+//    add(btnPentagon);
+//    add(btnHexagon);
 
     add(btnRed);
     add(btnGreen);
@@ -96,15 +117,15 @@ public class MainPage extends JFrame {
     add(labelTitle);
     add(labelShape);
     add(labelFillColor);
-    
+
     add(labelTranslate);
     add(labelInputTranslateX);
     add(labelInputTranslateY);
-    
+
     add(labelScale);
     add(labelInputScaleX);
     add(labelInputScaleY);
-    
+
     add(labelInputRotate);
   }
 
@@ -127,7 +148,7 @@ public class MainPage extends JFrame {
     labelInputTranslateY.setBounds(124, 156, 24, 28);
     inputTranslateY.setBounds(152, 156, 44, 28);
     btnTranslate.setBounds(208, 156, 96, 28);
-    
+
     labelScale.setBounds(340, 132, 240, 20);
     labelInputScaleX.setBounds(360, 156, 24, 28);
     inputScaleX.setBounds(388, 156, 44, 28);
@@ -140,21 +161,15 @@ public class MainPage extends JFrame {
     btnRotate.setBounds(208, 194, 96, 28);
 
     btnClear.setBounds(340, 194, 284, 28);
-
     panel.setBackground(new java.awt.Color(255, 255, 255));
     panel.setBounds(8, 240, 720, 680);
-  }
-
-  private void btnClearHandler(ActionEvent evt) {
-    panel.repaint();
-    g.setColor(Color.black);
   }
 
   private void btnRectangleHandler(ActionEvent evt) {
     setPanelBlank();
     lx = panel.getWidth() / 2 - 200 / 2;
-    ly = panel.getHeight()/ 2 - 120 / 2;
-    
+    ly = panel.getHeight() / 2 - 120 / 2;
+
     g.drawRect(lx, ly, 200, 120);
     State = "Rectangle";
   }
@@ -162,7 +177,7 @@ public class MainPage extends JFrame {
   private void btnOvalHandler(java.awt.event.ActionEvent evt) {
     setPanelBlank();
     lx = panel.getWidth() / 2 - 200 / 2;
-    ly = panel.getHeight()/ 2 - 120 / 2;
+    ly = panel.getHeight() / 2 - 120 / 2;
     g.drawOval(lx, ly, 200, 120);
     State = "Oval";
   }
@@ -170,22 +185,22 @@ public class MainPage extends JFrame {
   private void btnTriangleHandler(java.awt.event.ActionEvent evt) {
     setPanelBlank();
     lx = panel.getWidth() / 2 - 200 / 2;
-    ly = panel.getHeight()/ 2 - 120 / 2;
+    ly = panel.getHeight() / 2 - 120 / 2;
     int x[] = {lx, lx + 100, lx + 200};
     int y[] = {ly + 120, ly, ly + 120};
-    
+
     g.drawPolygon(x, y, 3);
     State = "Triangle";
   }
-  
+
   private void btnPentagonHandler(ActionEvent evt) {
     setPanelBlank();
-     lx = panel.getWidth() / 2 - 200 / 2;
-    ly = panel.getHeight()/ 2 - 120 / 2;
+    lx = panel.getWidth() / 2 - 200 / 2;
+    ly = panel.getHeight() / 2 - 120 / 2;
     System.out.println(ly);
     int x[] = {lx, lx + 100, lx + 200, lx + 300, lx + 400};
     int y[] = {ly + 120, ly, ly + 120, ly, ly + 120};
-    
+
     g.drawPolygon(x, y, 5);
     State = "Pentagon";
   }
@@ -193,39 +208,70 @@ public class MainPage extends JFrame {
   private void btnHexagonHandler(java.awt.event.ActionEvent evt) {
     setPanelBlank();
     lx = panel.getWidth() / 2 - 120 / 2;
-    ly = panel.getHeight()/ 2 - 120 / 2;
-    g.drawRect(lx, ly, 120 , 120);
-    State = "Square";;
+    ly = panel.getHeight() / 2 - 120 / 2;
+    g.drawRect(lx, ly, 120, 120);
+    State = "Hexagon";
   }
 
-  void setPanelBlank() {
-    g.setColor(Color.WHITE); // Set the color to white
-    g.fillRect(0, 0, panel.getWidth(), panel.getHeight()); // Clear the canvas
+  private void btnColorHandler(ActionEvent evt, Color color) {
+    setPanelBlank();
+    if (State != null) {
+      g.setColor(color);
+
+      switch (State) {
+        case "Rectangle":
+          g.fillRect(lx, ly, 200, 120);
+          break;
+        case "Oval":
+          g.fillOval(lx, ly, 200, 120);
+          break;
+        case "Triangle":
+          int x[] = {lx, lx + 100, lx + 200};
+          int y[] = {ly + 120, ly, ly + 120};
+          g.fillPolygon(x, y, x.length);
+          break;
+        default:
+          break;
+      }
+    } else {
+      JOptionPane.showMessageDialog(null, "Tidak ada bangun datar.");
+    }
+  }
+  private void btnClearHandler(ActionEvent evt) {
+    panel.repaint();
     g.setColor(Color.BLACK);
+    State = null;
+  }
+  
+
+  void setPanelBlank() {
+    g.setColor(Color.WHITE); // Set the fill color to white
+    g.fillRect(0, 0, panel.getWidth(), panel.getHeight()); // Clear the canvas
+    g.setColor(Color.BLACK); // Set outline color to black
   }
 
   private JPanel panel = new JPanel();
   private JButton btnClear = new JButton("Clear"), btnTranslate = new JButton("Translate"), btnRotate = new JButton("Rotate"), btnScale = new JButton("Scale");
   private JButton btnPentagon = new JButton("Pentagon"), btnRectangle = new JButton("Rectangle"), btnOval = new JButton("Oval"), btnTriangle = new JButton("Triangle"), btnHexagon = new JButton("Hexagon");
   private JButton btnRed = new JButton("Red"), btnGreen = new JButton("Green"), btnBlue = new JButton("Blue");
-  
+
   private final JTextField inputTranslateX = new JTextField();
   private final JTextField inputTranslateY = new JTextField();
   private final JTextField inputScaleX = new JTextField();
   private final JTextField inputScaleY = new JTextField();
   private final JTextField inputRotate = new JTextField();
-  
+
   private final JLabel labelTitle = new JLabel("Grafika Komputer");
   private final JLabel labelShape = new JLabel("Shape");
   private final JLabel labelFillColor = new JLabel("Fill Color");
-  
+
   private final JLabel labelScale = new JLabel("Scale (X, Y)");
   private final JLabel labelInputScaleX = new JLabel("X = ");
   private final JLabel labelInputScaleY = new JLabel("Y = ");
-  
+
   private final JLabel labelTranslate = new JLabel("Translate (X, Y)");
   private final JLabel labelInputTranslateX = new JLabel("X = ");
   private final JLabel labelInputTranslateY = new JLabel("Y = ");
-  
+
   private final JLabel labelInputRotate = new JLabel("Rotate");
 }
