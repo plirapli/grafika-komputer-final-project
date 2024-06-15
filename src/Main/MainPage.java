801fed7264975d442ac84ea3aaa9c9b8ab17983e
rafliyulistiawan <rafliyulistiawan@gmail>
2024-06-15 12:38:28 +0700

    rotate done

--- src/Main/MainPage.java
+++ src/Main/MainPage.java
@@ -2,6 +2,7 @@ package Main;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.*;
@@ -12,7 +13,7 @@ public class MainPage extends JFrame {
   String State = null;
   int lx, ly;
   int scaleX, scaleY;
-  Color currentFillColor = Color.white;
+  Color currentFillColor = null;
 
   private int rotationAngle = 0;
 
@@ -94,6 +95,18 @@ public class MainPage extends JFrame {
         btnTranslateHandler(e);
       }
     });
+    btnScale.addActionListener(new ActionListener() {
+      @Override
+      public void actionPerformed(ActionEvent e) {
+        btnScaleHandler(e);
+      }
+    });
+    btnRotate.addActionListener(new ActionListener() {
+      @Override
+      public void actionPerformed(ActionEvent e) {
+        btnRotateHandler(e);
+      }
+    });
   }
 
   private void addComponents() {
@@ -171,47 +184,50 @@ public class MainPage extends JFrame {
     panel.setBounds(8, 240, 720, 680);
   }
 
-  private void btnRectangleHandler(ActionEvent evt) {
+  private void btnRectangleHandler(ActionEvent event) {
     setPanelBlank();
+    resetRotation();
+    resetColor();
     lx = panel.getWidth() / 2 - 200 / 2;
     ly = panel.getHeight() / 2 - 120 / 2;
-
     g.drawRect(lx, ly, 200, 120);
     State = "Rectangle";
   }
 
-  private void btnOvalHandler(java.awt.event.ActionEvent evt) {
+  private void btnOvalHandler(java.awt.event.ActionEvent event) {
     setPanelBlank();
+    resetRotation();
+    resetColor();
     lx = panel.getWidth() / 2 - 200 / 2;
     ly = panel.getHeight() / 2 - 120 / 2;
     g.drawOval(lx, ly, 200, 120);
     State = "Oval";
   }
 
-  private void btnTriangleHandler(java.awt.event.ActionEvent evt) {
+  private void btnTriangleHandler(java.awt.event.ActionEvent event) {
     setPanelBlank();
+    resetRotation();
+    resetColor();
     lx = panel.getWidth() / 2 - 200 / 2;
     ly = panel.getHeight() / 2 - 120 / 2;
     int x[] = {lx, lx + 100, lx + 200};
     int y[] = {ly + 120, ly, ly + 120};
-
     g.drawPolygon(x, y, 3);
     State = "Triangle";
   }
 
-  private void btnPentagonHandler(ActionEvent evt) {
+  private void btnPentagonHandler(ActionEvent event) {
     setPanelBlank();
     lx = panel.getWidth() / 2 - 200 / 2;
     ly = panel.getHeight() / 2 - 120 / 2;
     System.out.println(ly);
     int x[] = {lx, lx + 100, lx + 200, lx + 300, lx + 400};
     int y[] = {ly + 120, ly, ly + 120, ly, ly + 120};
-
     g.drawPolygon(x, y, 5);
     State = "Pentagon";
   }
 
-  private void btnHexagonHandler(java.awt.event.ActionEvent evt) {
+  private void btnHexagonHandler(java.awt.event.ActionEvent event) {
     setPanelBlank();
     lx = panel.getWidth() / 2 - 120 / 2;
     ly = panel.getHeight() / 2 - 120 / 2;
@@ -219,23 +235,25 @@ public class MainPage extends JFrame {
     State = "Hexagon";
   }
 
-  private void btnColorHandler(ActionEvent evt, Color color) {
-    setPanelBlank();
+  private void btnColorHandler(ActionEvent event, Color color) {
     if (State != null) {
+      setPanelBlank();
       g.setColor(color);
+      currentFillColor = color;
 
       switch (State) {
-        case "Rectangle":
-          g.fillRect(lx, ly, 200, 120);
+        case "Rectangle": {
+          createRectangle();
           break;
-        case "Oval":
-          g.fillOval(lx, ly, 200, 120);
+        }
+        case "Oval": {
+          createOval();
           break;
-        case "Triangle":
-          int x[] = {lx, lx + 100, lx + 200};
-          int y[] = {ly + 120, ly, ly + 120};
-          g.fillPolygon(x, y, x.length);
+        }
+        case "Triangle": {
+          createTriangle();
           break;
+        }
         default:
           break;
       }
@@ -246,45 +264,75 @@ public class MainPage extends JFrame {
 
   private void btnTranslateHandler(ActionEvent event) {
     try {
-      setPanelBlank();
+      if (State == null) {
+        throw new Exception("Tidak ada bangun datar yang dipilih");
+      }
       String inputX = inputTranslateX.getText();
       String inputY = inputTranslateY.getText();
-
       if ("".equals(inputX) || "".equals(inputY)) {
         throw new Exception("Input masih kosong");
       }
 
       int tx = Integer.parseInt(inputTranslateX.getText());
       int ty = Integer.parseInt(inputTranslateY.getText());
-
-      if (State == null) {
-        throw new Exception("Tidak ada bangun datar yang dipilih");
-      }
-
       lx += tx;
       ly += ty;
+
+      setPanelBlank();
       switch (State) {
         case "Rectangle":
-          g.drawRect(lx, ly, 200, 120);
+          createRectangle();
           break;
         case "Oval":
-          g.drawOval(lx, ly, 200, 120);
+          createOval();
           break;
         case "Triangle":
-          int x[] = {lx, lx + 100, lx + 200};
-          int y[] = {ly + 120, ly, ly + 120};
-          g.drawPolygon(x, y, x.length);
+          createTriangle();
           break;
         default:
           break;
       }
-
     } catch (Exception e) {
       JOptionPane.showMessageDialog(null, e.getLocalizedMessage());
     }
   }
 
-  private void btnClearHandler(ActionEvent evt) {
+  private void btnRotateHandler(ActionEvent event) {
+    try {
+      if (State == null) {
+        throw new Exception("Tidak ada bangun datar yang dipilih");
+      }
+      String inputAngle = inputRotate.getText();
+      if ("".equals(inputAngle)) {
+        throw new Exception("Input masih kosong");
+      }
+
+      int angle = Integer.parseInt(inputAngle);
+      rotationAngle += angle;
+      setPanelBlank();
+      switch (State) {
+        case "Rectangle":
+          createRectangle();
+          break;
+        case "Oval":
+          createOval();
+          break;
+        case "Triangle":
+          createTriangle();
+          break;
+        default:
+          break;
+      }
+    } catch (Exception e) {
+      JOptionPane.showMessageDialog(null, e.getLocalizedMessage());
+    }
+  }
+
+  private void btnScaleHandler(ActionEvent event) {
+    System.out.println("Tes");
+  }
+
+  private void btnClearHandler(ActionEvent event) {
     panel.repaint();
     g.setColor(Color.BLACK);
     State = null;
@@ -294,6 +342,70 @@ public class MainPage extends JFrame {
     g.setColor(Color.WHITE); // Set the fill color to white
     g.fillRect(0, 0, panel.getWidth(), panel.getHeight()); // Clear the canvas
     g.setColor(Color.BLACK); // Set outline color to black
+  }
+
+  void resetRotation() {
+    rotationAngle = 0;
+  }
+
+  void resetColor() {
+    currentFillColor = null;
+  }
+
+  void createRectangle() {
+    Graphics2D graphics2D = (Graphics2D) g.create();
+    int width = 200;
+    int height = 120;
+
+    // Menghitung titik pusat
+    int centroidX = lx + (width / 2);
+    int centroidY = ly + (height / 2);
+
+    graphics2D.translate(centroidX, centroidY);
+    graphics2D.rotate(Math.toRadians(rotationAngle));
+    if (currentFillColor == null) {
+      graphics2D.drawRect(-width / 2, -height / 2, width, height);
+    } else {
+      graphics2D.setColor(currentFillColor);
+      graphics2D.fillRect(-width / 2, -height / 2, width, height);
+    }
+  }
+
+  void createOval() {
+    Graphics2D graphics2D = (Graphics2D) g.create();
+    int width = 200;
+    int height = 120;
+
+    // Menghitung titik pusat
+    int centroidX = lx + (width / 2);
+    int centroidY = ly + (height / 2);
+
+    graphics2D.translate(centroidX, centroidY);
+    graphics2D.rotate(Math.toRadians(rotationAngle));
+    if (currentFillColor == null) {
+      graphics2D.drawOval(-width / 2, -height / 2, width, height);
+    } else {
+      graphics2D.setColor(currentFillColor);
+      graphics2D.fillOval(-width / 2, -height / 2, width, height);
+    }
+  }
+
+  void createTriangle() {
+    Graphics2D graphics2D = (Graphics2D) g.create();
+    int x[] = {lx, lx + 100, lx + 200};
+    int y[] = {ly + 120, ly, ly + 120};
+
+    // Calculate the centroid of the triangle
+    int centroidX = (x[0] + x[1] + x[2]) / 3;
+    int centroidY = (y[0] + y[1] + y[2]) / 3;
+
+    // Rotate around the centroid
+    graphics2D.rotate(Math.toRadians(rotationAngle), centroidX, centroidY);
+    if (currentFillColor == null) {
+      graphics2D.drawPolygon(x, y, 3);
+    } else {
+      graphics2D.fillPolygon(x, y, 3);
+    }
   }
 
   private JPanel panel = new JPanel();
